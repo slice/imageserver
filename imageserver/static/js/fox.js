db599533b13f7bfdb1698411241c3ff817095684
sliceofcode <cheesy.fried.bacon@gmail.com>
2017-02-11 18:56:14 -0800

    view: remove size button
    clicking on the image itself will toggle the size

--- imageserver/static/js/fox.js
+++ imageserver/static/js/fox.js
@@ -1,11 +1,10 @@
 window.addEventListener('load', function() {
-  const fullButton = document.querySelector('#full-button');
   let image = document.querySelector('img.presentation');
 
   image.style.maxWidth = '80vw';
   image.style.maxHeight = '80vh';
 
-  fullButton.addEventListener('click', function() {
+  image.addEventListener('click', function() {
     if (image.style.maxWidth === '80vw') {
       image.style.maxWidth = '100%';
       image.style.maxHeight = '100%';
