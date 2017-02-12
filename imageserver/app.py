66ab0b1b2dc930444aaf2be6d2171053a885b28e
sliceofcode <cheesy.fried.bacon@gmail.com>
2017-02-12 00:08:33 -0800

    app: add a database api

--- imageserver/app.py
+++ imageserver/app.py
@@ -1,8 +1,9 @@
-from flask import Flask, Response, render_template, redirect
-from .database import Database
 import os
+import time
 from os import path
 from os.path import join
+from flask import Flask, Response, render_template, redirect
+from .database import Database
 
 def images_folder():
     # get the path to the images folder
@@ -31,6 +32,28 @@ def route_image_view(filename):
     return render_template('view.html',
         filename=filename, next_image=next_image,
         prev_image=prev_image)
+
+def ms():
+    return int(round(time.time() * 1000))
+
+@app.route('/api/database/refresh')
+def api_database_refresh():
+    begin = ms()
+    db.refresh()
+    end = ms()
+    return str(end - begin)
+
+@app.route('/api/database/redothumbs')
+def api_database_redothumbs():
+    from .generate_thumbnails import generate_thumbnails
+    begin = ms()
+    generate_thumbnails()
+    end = ms()
+    return str(end - begin)
+
+@app.route('/admin')
+def route_admin():
+    return render_template('admin.html')
 
 @app.route('/i/<string:filename>')
 def route_image_proxy(filename):
